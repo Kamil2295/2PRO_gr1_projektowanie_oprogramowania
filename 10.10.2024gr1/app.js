b77de9a68c947569c6553a812819173a14d70588
Kamil2295 <kamil6308942@gmail.com>
2024-10-10 15:19:42 +0200

    gr1

---
+++ 10.10.2024gr1/app.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body")
+
+const div = document.createElement("div")
+
+body.appendChild(div)
+
+div.style.width = "500px"
+div.style.height = "500px"
+div.style.border = "1px solid black"
+
+div.setAttribute("id", "testowe")
